94f9c4f78b642b3d5e632e3f7a292b245ed5677c
bulbul <bulbulpachauri21@gmail.com>
2025-10-12 23:02:50 +0530

    refactor: update API URL handling and improve error logging in postData function

--- my-project/src/utils/api.js
+++ my-project/src/utils/api.js
@@ -1,26 +1,25 @@
 import axios from 'axios';
-const apiUrl = import.meta.env.VITE_API_URL;
 
-export const postData = async (URL, formData)=> {
-    try{
-        const response = await fetch(apiUrl + URL,{
+
+export const postData = async (URL, formData) => {
+    try {
+        const response = await fetch(process.env.REACT_APP_API_URL + URL, {
             method: "POST",
             headers: {
-                'Authorization':   `Banner ${localStorage.getItem('token')}`, //Include your API key in the Authorization
+                'Authorization': `Banner ${localStorage.getItem('token')}`, //Include your API key in the Authorization
                 'Content-Type': 'application/json', //Adjust the content type as needed
             },
 
-            body:JSON.stringify(formData)
+            body: JSON.stringify(formData)
         });
-        
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        
+
         const data = await response.json();
         return { data };
-    } catch (error){
-        console.log(error);
-        throw error;
-    }
+    } catch (error) {
+        console.error("Error:", error);}
 }
+
